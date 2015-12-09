581b5879827c3563af747ce7eb795936f3c0cb4c
guduchango <gudu.chango@gmail.com>
2015-12-09 07:54:05 -0300

    se agrego el archivo .env a la partte de blog

--- blog/config/database.php
+++ blog/config/database.php
@@ -53,16 +53,16 @@ return [
         ],
 
         'mysql' => [
-            'driver'    => 'mysql',
-            'host' => 'localhost',
-            'database'  => 'blog2',
-            'username'  => 'root',
-            'password'  => '',
-            'charset'   => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix'    => '',
-            'strict'    => false,
-        ],
+             'driver'    => 'mysql',
+             'host'      => env('DB_HOST', 'localhost'),
+             'database'  => env('DB_DATABASE', 'forge'),
+             'username'  => env('DB_USERNAME', 'forge'),
+             'password'  => env('DB_PASSWORD', ''),
+             'charset'   => 'utf8',
+             'collation' => 'utf8_unicode_ci',
+             'prefix'    => '',
+             'strict'    => false,
+         ],
 
         'pgsql' => [
             'driver'   => 'pgsql',
